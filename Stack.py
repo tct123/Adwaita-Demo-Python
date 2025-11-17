14123ffcec24e2b9d67c9bec362a74dc2fb8e63a
tct123 <42028373+tct123@users.noreply.github.com>
2025-11-17 12:02:00 +0100

    .

--- Stack.py
+++ Stack.py
@@ -1,47 +1,49 @@
 import gi
 import sys
 
-gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw
 
 
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.hb=Adw.HeaderBar()
+        self.hb = Adw.HeaderBar()
         self.set_titlebar(self.hb)
 
-        a=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        a = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
-        b=Adw.ViewStack(vexpand=True)
+        b = Adw.ViewStack(vexpand=True)
         a.append(b)
 
-        b.add_titled(Gtk.Label(label="XOX"),"xox","Xox")
-        b.add_titled(Gtk.Label(label="LOL"),"lol","Lol")
+        b.add_titled(Gtk.Label(label="XOX"), "xox", "Xox")
+        b.add_titled(Gtk.Label(label="LOL"), "lol", "Lol")
 
-        self.d=Adw.ViewSwitcherTitle()
+        self.d = Adw.ViewSwitcherTitle()
         self.d.set_stack(b)
         self.d.set_title("LOL")
         self.hb.set_title_widget(self.d)
 
-        self.c=Adw.ViewSwitcherBar()
+        self.c = Adw.ViewSwitcherBar()
         self.c.set_stack(b)
         self.c.set_reveal(True)
         a.append(self.c)
 
         self.set_child(a)
-        self.d.connect("notify::title-visible",self.f)
+        self.d.connect("notify::title-visible", self.f)
 
-    def f(self,*data):
+    def f(self, *data):
         if self.d.get_title_visible():
             self.c.set_reveal(True)
         else:
             self.c.set_reveal(False)
+
+
 class MyApp(Adw.Application):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.connect('activate', self.on_activate)
+        self.connect("activate", self.on_activate)
 
     def on_activate(self, app):
         self.win = MainWindow(application=app)
@@ -52,5 +54,6 @@ def main(version):
     app = MyApp()
     return app.run(sys.argv)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main(None)
